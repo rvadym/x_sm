3e51023095f12654ad3f50ff444d8d535c734daa
vadym <vadym@xxx.(none)>
2013-06-14 12:42:09 +0300

    showMwssages

--- lib/Controller/ServMessages.php
+++ lib/Controller/ServMessages.php
@@ -24,20 +24,37 @@ class Controller_ServMessages extends \Controller {
 
         $this->showMwssages();
     }
-    function addMessage($key, $value) {
+    function addMessage($key, $type, $value) {
         $messages_arr = $this->recallMessages();
-        $messages_arr[$key] = $value;
+        $messages_arr[$key] = array('type'=>$type,'text'=>$value);
         $this->memorizeMessages($messages_arr);
     }
     function showMwssages() {
-
+        $messages_arr = $this->recallMessages();
+        if (!is_array($messages_arr)) return false;
+        foreach ($messages_arr as $message) {
+            switch ($message['type']) {
+                case 'success':
+                    $this->api->js(true)->univ()->successMessage($message['text']);
+                    break;
+                case 'error':
+                    $this->api->js(true)->univ()->errorMessage($message['text']);
+                    break;
+                default:
+                    throw $this->exception('There is no such a system message type!');
+            }
+        }
+        $this->forgetMessages();
     }
 
     ///  PRIVATE
     private function memorizeMessages($arr) {
-        $this->memorize($this->var_name,$arr);
+        $this->memorize($this->session_var_name,$arr);
     }
     private function recallMessages() {
-        return $this->recall($this->var_name);
+        return $this->recall($this->session_var_name);
+    }
+    private function forgetMessages() {
+        return $this->forget($this->session_var_name);
     }
 }
